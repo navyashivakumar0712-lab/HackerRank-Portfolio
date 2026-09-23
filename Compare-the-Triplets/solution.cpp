3cc1f739ae0d57d2e17e211a703c3bb7fabed16d
Navyashree.P.S <navyashivakumar0712@gmail.com>
2026-09-23 11:52:16 +0530

    Add Compare the Triplets solution

---
+++ Compare-the-Triplets/solution.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> compareTriplets(vector<int> a, vector<int> b) {
+    int alice = 0;
+    int bob = 0;
+
+    for (int i = 0; i < 3; i++) {
+        if (a[i] > b[i]) {
+            alice++;
+        }
+        else if (a[i] < b[i]) {
+            bob++;
+        }
+    }
+
+    return {alice, bob};
+}
